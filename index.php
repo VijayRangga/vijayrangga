85f4905af112985edf2199d50559c07ce858731b
VijayRangga <45963814+VijayRangga@users.noreply.github.com>
2018-12-19 20:39:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,23 @@
   <p><strong>Phone: 085 255 *** ***</strong></p>
   <p><strong>Fax: vijaysmada@gmail.com</strong></p>
   <p><strong> Sulawesi Selatan</strong></p></td>
+  <td width="70" align="center">
+
+
+    <script type="text/javascript"> 
+    // 1 detik = 1000 
+    window.setTimeout("waktu()",1000);   
+    function waktu() {    
+        var tanggal = new Date();   
+        setTimeout("waktu()",1000);   
+       document.getElementById("output").innerHTML = tanggal.getHours()+":"+tanggal.getMinutes()+":"+tanggal.getSeconds(); 
+   } 
+   </script> 
+   </head>
+   <body bgcolor="white" text="black" onLoad="waktu()"> 
+   <div id="output"></div> 
+
+
+</td>
 
 </table>
